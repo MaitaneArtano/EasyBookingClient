78e299f639a53eb52a3caf7283c45143cb4d557f
Ianire <Ianire@DBS17-0IDGGDH>
2018-05-09 08:28:25 +0200

    Prueba 3

--- EasyBookingClient/src/Cliente/Main.java
+++ EasyBookingClient/src/Cliente/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
 	public static void main(String[] args) 
 	{
-	System.out.println("Vamos RAFA");
-	System.out.println("vamooos");
-	System.out.println("prueba");
-	System.out.println("prueba2");
-
+		System.out.println("Vamos RAFA");
+		System.out.println("vamooos");
+		System.out.println("prueba");
+		System.out.println("prueba2");
+		System.out.println("prueba3");
 	}
 
 }
