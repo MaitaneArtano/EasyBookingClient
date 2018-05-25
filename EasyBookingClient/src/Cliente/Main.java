30832e0954d48a977d5ced98fdece29cf82cbca3
Ianire <ianire.andola@opendeusto.es>
2018-05-25 09:38:18 +0200

    Main

--- EasyBookingClient/src/Cliente/Main.java
+++ EasyBookingClient/src/Cliente/Main.java
@@ -1,14 +1,41 @@
 package Cliente;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import ControllerCliente.UsuarioController;
+import ControllerCliente.VueloController;
+import client.controller.ControllerCancion;
+import client.controller.ControllerPago;
+import client.controller.ControllerUsuario;
+import client.gui.GUIMenu;
+import RemoteCliente.ServiceLocator;
+
 public class Main {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Vamos RAFA");
-		System.out.println("vamooos");
-		System.out.println("prueba");
-		System.out.println("prueba2");
-		System.out.println("prueba3");
+		ServiceLocator serviceLocator = new ServiceLocator();
+		/*serviceLocator.setServiceUsuario(args[0], args[1],  args[2]);
+		UsuarioController irc = new UsuarioController(serviceLocator);
+		
+		serviceLocator.setServicePago(args[0], args[1], args[4]);
+		ControllerPago p = new ControllerPago(serviceLocator);
+		
+		serviceLocator.setServiceCancion(args[0], args[1], args[3]);
+		VueloController rlc = new VueloController(serviceLocator);
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException| UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			((Throwable) e).printStackTrace();
+		}
+		
+		GUIMenu mgui = new GUIMenu(irc, rlc, p);
+		mgui.setVisible(true);*/
+		
+		
 	}
 
 }
