099abb50b167a59e96dc76b43c3f91c155bc7424
Ainhoa Gauna <ainhoa.gauna@opendeusto.es>
2018-06-03 16:40:36 +0200

    listaGUI

--- EasyBookingClient/src/GUICliente/BuscarGUI.java
+++ EasyBookingClient/src/GUICliente/BuscarGUI.java
@@ -108,6 +108,8 @@ public class BuscarGUI extends JFrame
 					//Hemen supuestamente completeVueloDTO bat bueltauko luke eta hori beste lehion baten edo mostrau beharko litzeke eta erosteko aukera eman
 					miVueloDTO = vueloC.buscarVuelo(textFieldID_vuelo.getText());
 					JOptionPane.showMessageDialog(null, miVueloDTO.getDestino());
+					listaVuelosGUI objListaVuelos=new listaVuelosGUI();
+					objListaVuelos.setVisible(true);
 				} catch (RemoteException e1)
 				{
 					JOptionPane.showMessageDialog(null, "Introduce un id vuelo para la búsqueda");
