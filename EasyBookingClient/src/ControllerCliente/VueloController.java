c3a34f5ee2957335fca84ec3c3149fbe1ecdb95e
Ianire <ianire.andola@opendeusto.es>
2018-06-03 10:15:47 +0200

    Buscando vuelo por id

--- EasyBookingClient/src/ControllerCliente/VueloController.java
+++ EasyBookingClient/src/ControllerCliente/VueloController.java
@@ -1,5 +1,9 @@
 package ControllerCliente;
 
+import java.rmi.RemoteException;
+import java.util.List;
+
+import DTO.CompleteVueloDTO;
 import RemoteCliente.ServiceLocator;
 
 public class VueloController 
@@ -9,6 +13,13 @@ public class VueloController
 	public VueloController(ServiceLocator serviceLocator)
 	{
 		this.serviceLocator = serviceLocator;
+	}
+	
+	public CompleteVueloDTO buscarVuelo(String id_vuelo) throws RemoteException
+	{
+		CompleteVueloDTO vueloDetails = new CompleteVueloDTO();
+		vueloDetails = serviceLocator.getService().getVueloDetails(id_vuelo);
+		return vueloDetails;
 	}
 
 }
--- EasyBookingClient/src/GUICliente/BuscarGUI.java
+++ EasyBookingClient/src/GUICliente/BuscarGUI.java
@@ -2,14 +2,18 @@ package GUICliente;
 
 import ControllerCliente.UsuarioController;
 import ControllerCliente.VueloController;
+import DTO.CompleteVueloDTO;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JSpinner;
 import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
 import java.awt.event.ActionEvent;
-import javax.swing.JList;
+import javax.swing.JPanel;
 
 public class BuscarGUI extends JFrame
 {
@@ -18,62 +22,76 @@ public class BuscarGUI extends JFrame
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private static VueloController vueloC;
+	
 	private JTextField textFieldOrigen;
 	private JTextField textFieldDestino;
 	private JTextField textFieldIda;
 	private JTextField textFieldVuelta;
+	private JTextField textFieldID_vuelo;
+	CompleteVueloDTO miVueloDTO = new CompleteVueloDTO();
 	/**
 	 * Create the frame.
 	 */
 	public BuscarGUI(UsuarioController usuarioC, VueloController vueloC)
 	{
+		
+		
+		this.vueloC = vueloC;
 		setTitle("BUSCAR VUELOS");
 		setBounds(100, 100, 579, 438);
 		getContentPane().setLayout(null);
 		
+		
+		textFieldID_vuelo = new JTextField();
+		textFieldID_vuelo.setColumns(10);
+		textFieldID_vuelo.setBounds(53, 78, 146, 26);
+		getContentPane().add(textFieldID_vuelo);
+		
+		JLabel lblIdVuelo = new JLabel("Id Vuelo");
+		lblIdVuelo.setBounds(53, 50, 69, 20);
+		getContentPane().add(lblIdVuelo);
+		
 		JLabel lblOrigen = new JLabel("Origen");
-		lblOrigen.setBounds(53, 51, 69, 20);
+		lblOrigen.setBounds(53, 123, 69, 20);
 		getContentPane().add(lblOrigen);
 		
 		textFieldOrigen = new JTextField();
-		textFieldOrigen.setBounds(53, 87, 146, 26);
+		textFieldOrigen.setBounds(53, 156, 146, 26);
 		getContentPane().add(textFieldOrigen);
 		textFieldOrigen.setColumns(10);
 		
 		JLabel lblDestino = new JLabel("Destino");
-		lblDestino.setBounds(53, 129, 69, 20);
+		lblDestino.setBounds(53, 195, 69, 20);
 		getContentPane().add(lblDestino);
 		
 		textFieldDestino = new JTextField();
-		textFieldDestino.setBounds(53, 165, 146, 26);
+		textFieldDestino.setBounds(53, 217, 146, 26);
 		getContentPane().add(textFieldDestino);
 		textFieldDestino.setColumns(10);
 		
 		JLabel lblFechaIda = new JLabel("Fecha ida");
-		lblFechaIda.setBounds(53, 217, 69, 20);
+		lblFechaIda.setBounds(53, 256, 69, 20);
 		getContentPane().add(lblFechaIda);
 		
 		textFieldIda = new JTextField();
-		textFieldIda.setBounds(53, 253, 146, 26);
+		textFieldIda.setBounds(53, 277, 146, 26);
 		getContentPane().add(textFieldIda);
 		textFieldIda.setColumns(10);
 		
 		JLabel lblFechaVuelta = new JLabel("Fecha vuelta");
-		lblFechaVuelta.setBounds(264, 217, 98, 20);
+		lblFechaVuelta.setBounds(266, 256, 98, 20);
 		getContentPane().add(lblFechaVuelta);
 		
 		textFieldVuelta = new JTextField();
-		textFieldVuelta.setBounds(264, 253, 146, 26);
+		textFieldVuelta.setBounds(266, 277, 146, 26);
 		getContentPane().add(textFieldVuelta);
 		textFieldVuelta.setColumns(10);
 		
 		JLabel lblCantidad = new JLabel("Cantidad");
 		lblCantidad.setBounds(53, 316, 69, 20);
 		getContentPane().add(lblCantidad);
-		
-		JSpinner spinner = new JSpinner();
-		spinner.setBounds(131, 313, 32, 26);
-		getContentPane().add(spinner);
+
 
 		JButton btnBuscar = new JButton("BUSCAR");
 		btnBuscar.addActionListener(new ActionListener()
@@ -85,30 +103,21 @@ public class BuscarGUI extends JFrame
 				textFieldDestino.getText();
 				textFieldIda.getText();
 				textFieldVuelta.getText();				
-				spinner.getValue();
-				
-				//vuelo controllerrea jungoa eta hor si hay vuelos que coincidan devolvera una lista
-			/*	
-				correcto=vueloC.buscar(origen,destino,ida,vuelta);
-				
-				if(correcto==true)
+				try 
 				{
-					//ez dakit nola bueltatukoun lista ta gero or seleccionatzeko nola in
-					JOptionPane.showMessageDialog(null, "Busqueda con exito");
-				}
-				else
+					//Hemen supuestamente completeVueloDTO bat bueltauko luke eta hori beste lehion baten edo mostrau beharko litzeke eta erosteko aukera eman
+					miVueloDTO = vueloC.buscarVuelo(textFieldID_vuelo.getText());
+					JOptionPane.showMessageDialog(null, miVueloDTO.getDestino());
+				} catch (RemoteException e1)
 				{
-					JOptionPane.showMessageDialog(null, "Busqueda fallido. Vuelva a intentarlo");
-				
+					JOptionPane.showMessageDialog(null, "Introduce un id vuelo para la búsqueda");
 				}
-				*/
+				
 			}
 				
 			
 		});
-		
 		btnBuscar.setBounds(320, 332, 115, 29);
 		getContentPane().add(btnBuscar);
-		
 	}
 }
